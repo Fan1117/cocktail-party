5fe943471c5520d0c2d0c5106260290e059cf86f
avivga <avivga@gmail.com>
2017-08-13 22:44:14 +0300

    refactor speech enhancer

--- speech_enhancer.py
+++ speech_enhancer.py
@@ -1,7 +1,6 @@
 import argparse
 import os
 import shutil
-import glob
 import random
 from datetime import datetime
 
@@ -12,65 +11,72 @@ from dsp.spectrogram import MelConverter
 from dataset import AudioVisualDataset
 
 
-def enhance(dataset_dir, speaker_id, noise_dir, speech_prediction_dir, enhancement_output_dir, enhancement_threshold, max_pairs=20):
-	dataset = AudioVisualDataset(dataset_dir)
-	speaker_files = dataset.subset([speaker_id], shuffle=True).audio_paths()
-	noise_files = glob.glob(os.path.join(noise_dir, "*.wav"))
+def enhance_speech(speaker_file_path, noise_file_path, speech_prediction_path, enhancement_threshold):
+	print("enhancing mix of %s, %s" % (speaker_file_path, noise_file_path))
 
-	random.shuffle(speaker_files)
-	random.shuffle(noise_files)
+	speaker_source_signal = AudioSignal.from_wav_file(speaker_file_path)
+	noise_source_signal = AudioSignal.from_wav_file(noise_file_path)
+	predicted_speech_signal = AudioSignal.from_wav_file(speech_prediction_path)
 
-	source_file_pairs = zip(speaker_files, noise_files)[:max_pairs]
+	while noise_source_signal.get_number_of_samples() < speaker_source_signal.get_number_of_samples():
+		noise_source_signal = AudioSignal.concat([noise_source_signal, noise_source_signal])
 
+	noise_source_signal = noise_source_signal.slice(0, speaker_source_signal.get_number_of_samples())
+
+	mixed_signal = AudioMixer.mix([speaker_source_signal, noise_source_signal])
+
+	signals = [mixed_signal, predicted_speech_signal]
+	max_length = max([signal.get_number_of_samples() for signal in signals])
+	for signal in signals:
+		signal.pad_with_zeros(max_length)
+
+	mel_converter = MelConverter(mixed_signal.get_sample_rate(), n_mel_freqs=128, freq_min_hz=300, freq_max_hz=3400)
+	mixed_spectrogram = mel_converter.signal_to_mel_spectrogram(mixed_signal)
+	predicted_speech_spectrogram = mel_converter.signal_to_mel_spectrogram(predicted_speech_signal)
+
+	speech_enhancement_mask = np.zeros(shape=mixed_spectrogram.shape)
+	speech_enhancement_mask[predicted_speech_spectrogram > enhancement_threshold] = 1
+
+	enhanced_speech_spectrogram = mixed_spectrogram * speech_enhancement_mask
+	enhanced_speech_signal = mel_converter.reconstruct_signal_from_mel_spectrogram(enhanced_speech_spectrogram)
+
+	return mixed_signal, enhanced_speech_signal
+
+
+def apply_speech_enhancement(dataset_dir, speaker_id, noise_dir, prediction_input_dir, enhancement_output_dir, enhancement_threshold):
 	enhancement_output_dir = os.path.join(enhancement_output_dir, '{:%Y-%m-%d_%H-%M-%S}'.format(datetime.now()))
 	os.mkdir(enhancement_output_dir)
 
-	for speaker_file_path, noise_file_path in source_file_pairs:
+	for speaker_file_path, noise_file_path in list_source_pairs(dataset_dir, speaker_id, noise_dir):
 		try:
-			print("enhancing mix of %s, %s" % (speaker_file_path, noise_file_path))
-
-			speaker_source_signal = AudioSignal.from_wav_file(speaker_file_path)
-			noise_source_signal = AudioSignal.from_wav_file(noise_file_path)
-
-			while noise_source_signal.get_number_of_samples() < speaker_source_signal.get_number_of_samples():
-				noise_source_signal = AudioSignal.concat([noise_source_signal, noise_source_signal])
-
-			noise_source_signal = noise_source_signal.slice(0, speaker_source_signal.get_number_of_samples())
-
-			mixed_signal = AudioMixer.mix([speaker_source_signal, noise_source_signal])
-
-			mel_converter = MelConverter(mixed_signal.get_sample_rate(), n_mel_freqs=128, freq_min_hz=300, freq_max_hz=3400)
-			mixed_spectrogram = mel_converter.signal_to_mel_spectrogram(mixed_signal)
-
 			speaker_file_name = os.path.splitext(os.path.basename(speaker_file_path))[0]
 			noise_file_name = os.path.splitext(os.path.basename(noise_file_path))[0]
-
-			speech_prediction_path = glob.glob(os.path.join(speech_prediction_dir, speaker_id, speaker_file_name + ".wav"))
-
-			predicted_speech_signal = AudioSignal.from_wav_file(speech_prediction_path[0])
-			predicted_speech_spectrogram = mel_converter.signal_to_mel_spectrogram(predicted_speech_signal)
-
-			if mixed_spectrogram.shape[1] > predicted_speech_spectrogram.shape[1]:
-				mixed_spectrogram = mixed_spectrogram[:, :predicted_speech_spectrogram.shape[1]]
-			else:
-				predicted_speech_spectrogram = predicted_speech_spectrogram[:, :mixed_spectrogram.shape[1]]
-
-			speech_enhancement_mask = np.zeros(shape=mixed_spectrogram.shape)
-			speech_enhancement_mask[predicted_speech_spectrogram > enhancement_threshold] = 1
-
-			enhanced_speech_spectrogram = mixed_spectrogram * speech_enhancement_mask
-
-			reconstructed_speech_signal = mel_converter.reconstruct_signal_from_mel_spectrogram(enhanced_speech_spectrogram)
 
 			speech_enhancement_dir_path = os.path.join(enhancement_output_dir, speaker_file_name + "_" + noise_file_name)
 			os.mkdir(speech_enhancement_dir_path)
 
-			reconstructed_speech_signal.save_to_wav_file(os.path.join(speech_enhancement_dir_path, "enhanced.wav"))
+			speech_prediction_path = os.path.join(prediction_input_dir, speaker_id, speaker_file_name + ".wav")
+			mixed_signal, enhanced_speech_signal = enhance_speech(
+				speaker_file_path, noise_file_path, speech_prediction_path, enhancement_threshold
+			)
+
+			shutil.copy(speaker_file_path, os.path.join(speech_enhancement_dir_path, "source.wav"))
+			enhanced_speech_signal.save_to_wav_file(os.path.join(speech_enhancement_dir_path, "enhanced.wav"))
 			mixed_signal.save_to_wav_file(os.path.join(speech_enhancement_dir_path, "mixture.wav"))
-			speaker_source_signal.save_to_wav_file(os.path.join(speech_enhancement_dir_path, "source.wav"))
 
 		except Exception as e:
 			print("failed to enhance (%s). skipping" % e)
+
+
+def list_source_pairs(dataset_dir, speaker_id, noise_dir):
+	dataset = AudioVisualDataset(dataset_dir)
+	speaker_file_paths = dataset.subset([speaker_id], max_files=20, shuffle=True).audio_paths()
+	noise_file_paths = [os.path.join(noise_dir, f) for f in os.listdir(noise_dir)]
+
+	random.shuffle(speaker_file_paths)
+	random.shuffle(noise_file_paths)
+
+	return zip(speaker_file_paths, noise_file_paths)
 
 
 def main():
@@ -78,14 +84,14 @@ def main():
 	parser.add_argument("dataset_dir", type=str)
 	parser.add_argument("speaker", type=str)
 	parser.add_argument("noise_dir", type=str)
-	parser.add_argument("speech_prediction_dir", type=str)
+	parser.add_argument("prediction_input_dir", type=str)
 	parser.add_argument("enhancement_output_dir", type=str)
 	parser.add_argument("threshold", type=int)
 	args = parser.parse_args()
 
-	enhance(
-		args.dataset_dir, args.speaker, args.noise_dir, args.speech_prediction_dir,
-		args.enhancement_output_dir, enhancement_threshold=args.threshold
+	apply_speech_enhancement(
+		args.dataset_dir, args.speaker, args.noise_dir, args.prediction_input_dir,
+		args.enhancement_output_dir, args.threshold
 	)
 
 
