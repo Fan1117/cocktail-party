4236b4f453a5255cd38b7e1ec86cbd6b7f4413c8
avivga <avivga@gmail.com>
2017-08-14 16:10:04 +0300

    audio-visual dataset wrapper: use audio list as internal member

--- dataset.py
+++ dataset.py
@@ -9,15 +9,15 @@ class AudioVisualDataset:
 		self._base_path = base_path
 
 	def subset(self, speaker_ids, max_files=None, shuffle=False):
-		video_file_paths = []
+		audio_file_paths = []
 
 		for speaker_id in speaker_ids:
-			video_file_paths.extend(glob.glob(os.path.join(self._base_path, speaker_id, "video", "*.mpg")))
+			audio_file_paths.extend(glob.glob(os.path.join(self._base_path, speaker_id, "audio", "*.wav")))
 
 		if shuffle:
-			random.shuffle(video_file_paths)
+			random.shuffle(audio_file_paths)
 
-		return AudioVisualSubset(video_file_paths[:max_files])
+		return AudioVisualSubset(audio_file_paths[:max_files])
 
 	def list_speakers(self):
 		return os.listdir(self._base_path)
@@ -25,15 +25,15 @@ class AudioVisualDataset:
 
 class AudioVisualSubset:
 
-	def __init__(self, video_file_paths):
-		self._video_file_paths = video_file_paths
-
-	def video_paths(self):
-		return self._video_file_paths
+	def __init__(self, audio_file_paths):
+		self._audio_file_paths = audio_file_paths
 
 	def audio_paths(self):
-		return [self._video_to_audio_path(v) for v in self._video_file_paths]
+		return self._audio_file_paths
+
+	def video_paths(self):
+		return [self._audio_to_video_path(a) for a in self._audio_file_paths]
 
 	@staticmethod
-	def _video_to_audio_path(video_file_path):
-		return video_file_path.replace("video", "audio").replace(".mpg", ".wav")
+	def _audio_to_video_path(audio_file_path):
+		return audio_file_path.replace("audio", "video").replace(".wav", ".mpg")
